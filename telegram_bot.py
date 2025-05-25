e5a3a60df12f733f6994db1e13ff32ea404a2874
Your Name <you@example.com>
2025-05-25 17:44:54 +0300

    25.05.2025

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,9 +1,11 @@
 import telebot
 import key_boards
 import fsm
+import ai
 
 bot_token = '7890554839:AAEE2cQU30PclJXCcetfBzVBo9ZVIOgz0aA'
 stater = fsm.FSM()
+al_service = ai.AI()
 bot = telebot.TeleBot(bot_token)
 
 def handle_default_state(message):
@@ -19,12 +21,22 @@ def handle_image_state(message):
     if message.text == 'меню':
         return_to_menu(message.chat.id)
     else:
-        bot.send_message(message.chat.id, text = 'Делаем')
+        try:
+            msg = bot.send_message(chat_id = message.chat.id, text = 'Генерируем' )
+            image_url = al_service.generate_image(message.text)
+            bot.delete_message(chat_id = message.chat.id, message_id = msg.id)
+            bot.send_photo(chat_id = message.chat.id, caption = 'Ваше фото', photo = image_url )
+        except Exception as e:
+            bot.send_message(chat_id =message.chat.id, text = f'Произошла ошибка({str(e)})')
 def handle_text_state(message):
     if message.text == 'меню':
+        al_service.clear_dialog(message.chat.id)
         return_to_menu(message.chat.id)
     else:
-        bot.send_message(message.chat.id, text = 'Делаем')
+        msg = bot.send_message(message.chat.id, 'Думаю над запросом')
+        txt = al_service.generate_text(message.text, message.chat.id)
+        msg = bot.edit_message_text(text = txt, chat_id = message.chat.id, message_id = msg.id)
+        
 def return_to_menu(chat_id):
     stater.set_state(chat_id,fsm.default_state)
     bot.send_message(chat_id, text = 'Главное меню:', reply_markup = key_boards.keyboard1)
